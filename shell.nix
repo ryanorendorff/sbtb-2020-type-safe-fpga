1244208f68879f0ddffa8b09fcb08bcec76111c7
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2020-11-15 23:29:41 -0700

    nixfmt shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,7 @@
 let
 
   clash-compiler = fetchTarball {
-    url = "https://github.com/clash-lang/clash-compiler/archive/v1.2.4.tar.gz"; 
+    url = "https://github.com/clash-lang/clash-compiler/archive/v1.2.4.tar.gz";
     sha256 = "0dc62aijrkacfjlckv461pj6i8h8mj7qry09mikxh6jl077a9wlz";
   };
-in 
-  import "${clash-compiler}/shell.nix"
+in import "${clash-compiler}/shell.nix"
